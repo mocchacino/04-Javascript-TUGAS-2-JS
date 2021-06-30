f8381608e94285e5bcf9064dd69e5b531c11bd9b
wiladhianty <wiladhianty@gmail.com>
2021-06-30 10:55:43 +0700

    tambah tugas

---
+++ tugas2.js
@@ -0,0 +1,13 @@
+var angka = 100;
+var kata = "sapi";
+var benar = true;
+var nul = null;
+var kosong = undefined;
+var alfabet = ['a','b','c','d'];
+
+console.log(angka)
+console.log(kata)
+console.log(benar)
+console.log(nul)
+console.log(kosong)
+console.log(alfabet)
